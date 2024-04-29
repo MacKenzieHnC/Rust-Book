11db93486061b744fecf57c65c446c0bf0505910
MacKenzie H&C <mackenziehnc@gmail.com>
2024-04-29 11:54:19 -0500

    get height from user

--- Personal-Projects/peaucellier-lipkin-linkage/src/main.rs
+++ Personal-Projects/peaucellier-lipkin-linkage/src/main.rs
@@ -1,5 +1,17 @@
 use nalgebra::Point2;
+use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    println!("How tall is the linkage?");
+
+    let mut h = String::new();
+
+    io::stdin().read_line(&mut h).expect("Failed to read line");
+
+    let h: f32 = match h.trim().parse() {
+        Ok(num) => num,
+        Err(e) => panic!("h is not a number: {:?}", e),
+    };
+
+    println!("The linkage is {h} tall!")
 }
